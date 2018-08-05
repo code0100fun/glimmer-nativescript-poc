34f3b1407438a5c6948e81a1abd69b59041f7dbf
Chase McCarthy <chase@code0100fun.com>
2018-08-04 19:39:59 -0500

    Add tap handler to hamburger to open side drawer

--- src/ui/components/Home/component.js
+++ src/ui/components/Home/component.js
@@ -1,5 +1,9 @@
 import Component, { tracked } from "@glimmer/component";
+import * as app from "application";
 
 export default class Home extends Component {
-
+  toggleMenu() {
+    let sideDrawer = app.getRootView();
+    sideDrawer.showDrawer();
+  }
 }
